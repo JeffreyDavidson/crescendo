77bd8cbdf724e3564cc0ea0e766419afd6ccf803
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2020-10-24 10:36:51 -0400

    Update database seeder namespace for Laravel 8

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Arranger;
+use App\Category;
+use App\Composer;
+use App\Instrument;
+use App\MusicPiece;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+
+class DatabaseSeeder extends Seeder
+{
+    protected $toTruncate = [
+        'musical_pieces',
+        'composers',
+        'arrangers',
+        'instruments',
+        'composer_music_piece',
+        'arranger_music_piece',
+        'instrument_music_piece',
+    ];
+
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (Category::count() == 0) {
+            Category::create(['name' => 'Flute']);
+            Category::create(['name' => 'Piano']);
+            Category::create(['name' => 'Hand Bells']);
+        }
+
+        Model::unguard();
+
+        foreach ($this->toTruncate as $table) {
+            DB::table($table)->truncate();
+        }
+
+        MusicPiece::factory()
+                ->has(Composer::factory()->count(3))
+                ->has(Arranger::factory()->count(3))
+                ->has(Instrument::factory()->count(3))
+                ->count(50)
+                ->create(['category_id' => Category::inRandomOrder()->first()->id]);
+
+        Model::reguard();
+    }
+}
