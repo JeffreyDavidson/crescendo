6aa96e8a6f350b454c7b0de68a7d7bc0f7b24f2c
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2020-10-23 22:48:57 -0400

    Modify order of links for navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,18 +1,18 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Crescendo</a>
+    <h1 class="text-white">Crescendo</h1>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
+      <ul class="ml-4 navbar-nav">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Hand Bells <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Flute</a>
+          <a class="nav-link" href="#">Flute <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#">Piano</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Hand Bells</a>
         </li>
       </ul>
     </div>
